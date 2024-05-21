2eee6b71946c276d01771016dac2b7ca98597365
Shimamura Yuta <shimamura@agora-c.com>
2024-05-21 16:47:34 +0900

    20240521 commit

--- lib/screens/plan_timeline.dart
+++ lib/screens/plan_timeline.dart
@@ -2,12 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:kintaikei_web/common/functions.dart';
 import 'package:kintaikei_web/common/style.dart';
+import 'package:kintaikei_web/models/plan.dart';
 import 'package:kintaikei_web/providers/home.dart';
 import 'package:kintaikei_web/providers/login.dart';
-import 'package:kintaikei_web/screens/plan_add.dart';
-import 'package:kintaikei_web/screens/plan_mod.dart';
+import 'package:kintaikei_web/providers/plan.dart';
+import 'package:kintaikei_web/services/date_time_picker.dart';
 import 'package:kintaikei_web/services/plan.dart';
+import 'package:kintaikei_web/widgets/custom_button_sm.dart';
+import 'package:kintaikei_web/widgets/custom_text_box.dart';
 import 'package:kintaikei_web/widgets/custom_timeline.dart';
+import 'package:kintaikei_web/widgets/datetime_range_form.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart' as sfc;
 
 class PlanTimelineScreen extends StatefulWidget {
@@ -76,26 +81,22 @@ class _PlanTimelineScreenState extends State<PlanTimelineScreen> {
                   case sfc.CalendarElement.agenda:
                     sfc.Appointment appointmentDetails =
                         details.appointments![0];
-                    Navigator.push(
-                      context,
-                      FluentPageRoute(
-                        builder: (context) => PlanModScreen(
-                          loginProvider: widget.loginProvider,
-                          homeProvider: widget.homeProvider,
-                          planId: '${appointmentDetails.id}',
-                        ),
+                    showDialog(
+                      context: context,
+                      builder: (context) => ModPlanDialog(
+                        loginProvider: widget.loginProvider,
+                        homeProvider: widget.homeProvider,
+                        planId: '${appointmentDetails.id}',
                       ),
                     );
                     break;
                   case sfc.CalendarElement.calendarCell:
-                    Navigator.push(
-                      context,
-                      FluentPageRoute(
-                        builder: (context) => PlanAddScreen(
-                          loginProvider: widget.loginProvider,
-                          homeProvider: widget.homeProvider,
-                          date: details.date ?? DateTime.now(),
-                        ),
+                    showDialog(
+                      context: context,
+                      builder: (context) => AddPlanDialog(
+                        loginProvider: widget.loginProvider,
+                        homeProvider: widget.homeProvider,
+                        date: details.date ?? DateTime.now(),
                       ),
                     );
                     break;
@@ -114,5 +115,428 @@ class _PlanTimelineScreenState extends State<PlanTimelineScreen> {
 class _DataSource extends sfc.CalendarDataSource {
   _DataSource(List<sfc.Appointment> source) {
     appointments = source;
+  }
+}
+
+class AddPlanDialog extends StatefulWidget {
+  final LoginProvider loginProvider;
+  final HomeProvider homeProvider;
+  final DateTime date;
+
+  const AddPlanDialog({
+    required this.loginProvider,
+    required this.homeProvider,
+    required this.date,
+    super.key,
+  });
+
+  @override
+  State<AddPlanDialog> createState() => _AddPlanDialogState();
+}
+
+class _AddPlanDialogState extends State<AddPlanDialog> {
+  DateTimePickerService pickerService = DateTimePickerService();
+  TextEditingController subjectController = TextEditingController();
+  DateTime startedAt = DateTime.now();
+  DateTime endedAt = DateTime.now();
+  bool allDay = false;
+  Color color = kColors.first;
+  int alertMinute = kAlertMinutes[1];
+
+  void _init() async {
+    startedAt = widget.date;
+    endedAt = startedAt.add(const Duration(hours: 1));
+    setState(() {});
+  }
+
+  void _allDayChange(bool? value) {
+    allDay = value ?? false;
+    if (allDay) {
+      startedAt = DateTime(
+        startedAt.year,
+        startedAt.month,
+        startedAt.day,
+        0,
+        0,
+        0,
+      );
+      endedAt = DateTime(
+        endedAt.year,
+        endedAt.month,
+        endedAt.day,
+        23,
+        59,
+        59,
+      );
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final planProvider = Provider.of<PlanProvider>(context);
+    return ContentDialog(
+      constraints: const BoxConstraints(
+        maxWidth: 600,
+        maxHeight: 500,
+      ),
+      title: const Text(
+        '予定を新しく追加',
+        style: TextStyle(fontSize: 18),
+      ),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InfoLabel(
+              label: '件名',
+              child: CustomTextBox(
+                controller: subjectController,
+                placeholder: '',
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+              ),
+            ),
+            const SizedBox(height: 8),
+            InfoLabel(
+              label: '予定時間帯を設定',
+              child: DatetimeRangeForm(
+                startedAt: startedAt,
+                startedOnTap: () async => await pickerService.boardPicker(
+                  context: context,
+                  init: startedAt,
+                  title: '予定開始日時を選択',
+                  onChanged: (value) {
+                    setState(() {
+                      startedAt = value;
+                      endedAt = startedAt.add(const Duration(hours: 1));
+                    });
+                  },
+                ),
+                endedAt: endedAt,
+                endedOnTap: () async => await pickerService.boardPicker(
+                  context: context,
+                  init: endedAt,
+                  title: '予定終了日時を選択',
+                  onChanged: (value) {
+                    setState(() {
+                      endedAt = value;
+                    });
+                  },
+                ),
+                allDay: allDay,
+                allDayOnChanged: _allDayChange,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Expander(
+              header: const Text('詳細設定'),
+              content: Column(
+                children: [
+                  InfoLabel(
+                    label: '色',
+                    child: ComboBox<Color>(
+                      isExpanded: true,
+                      value: color,
+                      items: kColors.map((value) {
+                        return ComboBoxItem(
+                          value: value,
+                          child: Container(
+                            color: value,
+                            height: 25,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          color = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  InfoLabel(
+                    label: '事前アラート通知',
+                    child: ComboBox<int>(
+                      isExpanded: true,
+                      value: alertMinute,
+                      items: kAlertMinutes.map((value) {
+                        return ComboBoxItem(
+                          value: value,
+                          child:
+                              value == 0 ? const Text('無効') : Text('$value分前'),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          alertMinute = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        CustomButtonSm(
+          labelText: 'キャンセル',
+          labelColor: kWhiteColor,
+          backgroundColor: kGreyColor,
+          onPressed: () => Navigator.pop(context),
+        ),
+        CustomButtonSm(
+          labelText: '上記内容で追加する',
+          labelColor: kWhiteColor,
+          backgroundColor: kBlueColor,
+          onPressed: () async {
+            String? error = await planProvider.create(
+              company: widget.loginProvider.company,
+              group: widget.homeProvider.currentGroup,
+              subject: subjectController.text,
+              startedAt: startedAt,
+              endedAt: endedAt,
+              allDay: allDay,
+              color: color,
+              alertMinute: alertMinute,
+            );
+            if (error != null) {
+              if (!mounted) return;
+              showMessage(context, error, false);
+              return;
+            }
+            if (!mounted) return;
+            showMessage(context, '予定を追加しました', true);
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ModPlanDialog extends StatefulWidget {
+  final LoginProvider loginProvider;
+  final HomeProvider homeProvider;
+  final String planId;
+
+  const ModPlanDialog({
+    required this.loginProvider,
+    required this.homeProvider,
+    required this.planId,
+    super.key,
+  });
+
+  @override
+  State<ModPlanDialog> createState() => _ModPlanDialogState();
+}
+
+class _ModPlanDialogState extends State<ModPlanDialog> {
+  PlanService planService = PlanService();
+  DateTimePickerService pickerService = DateTimePickerService();
+  TextEditingController subjectController = TextEditingController();
+  DateTime startedAt = DateTime.now();
+  DateTime endedAt = DateTime.now();
+  bool allDay = false;
+  Color color = kColors.first;
+  int alertMinute = kAlertMinutes[1];
+
+  void _init() async {
+    PlanModel? plan = await planService.selectDataToId(id: widget.planId);
+    if (plan == null) {
+      if (!mounted) return;
+      showMessage(context, '予定データの取得に失敗しました', false);
+      Navigator.of(context, rootNavigator: true).pop();
+      return;
+    }
+    subjectController.text = plan.subject;
+    startedAt = plan.startedAt;
+    endedAt = plan.endedAt;
+    allDay = plan.allDay;
+    color = plan.color;
+    alertMinute = plan.alertMinute;
+    setState(() {});
+  }
+
+  void _allDayChange(bool? value) {
+    allDay = value ?? false;
+    if (allDay) {
+      startedAt = DateTime(
+        startedAt.year,
+        startedAt.month,
+        startedAt.day,
+        0,
+        0,
+        0,
+      );
+      endedAt = DateTime(
+        endedAt.year,
+        endedAt.month,
+        endedAt.day,
+        23,
+        59,
+        59,
+      );
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final planProvider = Provider.of<PlanProvider>(context);
+    return ContentDialog(
+      constraints: const BoxConstraints(
+        maxWidth: 600,
+        maxHeight: 500,
+      ),
+      title: const Text(
+        '予定を編集',
+        style: TextStyle(fontSize: 18),
+      ),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InfoLabel(
+              label: '件名',
+              child: CustomTextBox(
+                controller: subjectController,
+                placeholder: '',
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+              ),
+            ),
+            const SizedBox(height: 8),
+            InfoLabel(
+              label: '予定時間帯を設定',
+              child: DatetimeRangeForm(
+                startedAt: startedAt,
+                startedOnTap: () async => await pickerService.boardPicker(
+                  context: context,
+                  init: startedAt,
+                  title: '予定開始日時を選択',
+                  onChanged: (value) {
+                    setState(() {
+                      startedAt = value;
+                      endedAt = startedAt.add(const Duration(hours: 1));
+                    });
+                  },
+                ),
+                endedAt: endedAt,
+                endedOnTap: () async => await pickerService.boardPicker(
+                  context: context,
+                  init: endedAt,
+                  title: '予定終了日時を選択',
+                  onChanged: (value) {
+                    setState(() {
+                      endedAt = value;
+                    });
+                  },
+                ),
+                allDay: allDay,
+                allDayOnChanged: _allDayChange,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Expander(
+              header: const Text('詳細設定'),
+              content: Column(
+                children: [
+                  InfoLabel(
+                    label: '色',
+                    child: ComboBox<Color>(
+                      isExpanded: true,
+                      value: color,
+                      items: kColors.map((value) {
+                        return ComboBoxItem(
+                          value: value,
+                          child: Container(
+                            color: value,
+                            height: 25,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          color = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  InfoLabel(
+                    label: '事前アラート通知',
+                    child: ComboBox<int>(
+                      isExpanded: true,
+                      value: alertMinute,
+                      items: kAlertMinutes.map((value) {
+                        return ComboBoxItem(
+                          value: value,
+                          child:
+                              value == 0 ? const Text('無効') : Text('$value分前'),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          alertMinute = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        CustomButtonSm(
+          labelText: 'キャンセル',
+          labelColor: kWhiteColor,
+          backgroundColor: kGreyColor,
+          onPressed: () => Navigator.pop(context),
+        ),
+        CustomButtonSm(
+          labelText: '上記内容で保存する',
+          labelColor: kWhiteColor,
+          backgroundColor: kBlueColor,
+          onPressed: () async {
+            String? error = await planProvider.update(
+              id: widget.planId,
+              subject: subjectController.text,
+              startedAt: startedAt,
+              endedAt: endedAt,
+              allDay: allDay,
+              color: color,
+              alertMinute: alertMinute,
+            );
+            if (error != null) {
+              if (!mounted) return;
+              showMessage(context, error, false);
+              return;
+            }
+            if (!mounted) return;
+            showMessage(context, '予定を編集しました', true);
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
   }
 }
